86c20ee59f40a4782d320409489e4bbd1005907e
Ben Wellingstein <benwellingstein@gmail.com>
2017-12-27 09:36:12 +0100

    Initial Commit

---
+++ theAwesomenessContinues/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  theAwesomenessContinues
+//
+//  Created by Ben on 27/12/2017.
+//  Copyright © 2017 Ben. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
